9fbe94ed7616cae8aef4a8df2dcf47141babe0f3
Dann <kentplaysgg@gmail.com>
2025-12-22 00:13:20 -0300

    AÇOES FRONT

--- frontend/src/pages/Produto.jsx
+++ frontend/src/pages/Produto.jsx
@@ -1,109 +1,206 @@
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
-import { Table,Checkbox,Button,Menu, MenuDropdown, MenuLabel } from '@mantine/core';
-import { IconCheck } from '@tabler/icons-react';
+import { Table, Checkbox, Button, Menu, MenuDropdown, MenuLabel } from '@mantine/core';
+import { IconCheck, IconX } from '@tabler/icons-react';
 import { showNotification } from "@mantine/notifications";
 import Navbar from '../components/navbar';
 import './Produto.css'
+import ArchiveIcon from '../components/icons/ArchiveIcons'
 function Produto() {
   const [produtos, setProdutos] = useState([]);
 
-  
+
 
   useEffect(() => {
     fetch("http://127.0.0.1:8000/produtos/")
-    .then(res =>{
-    if(!res.ok){
-      throw new Error("Erro HTTP" + res.status);
-    }
-      return res.json();
-    })
-    .then(data => setProdutos(data.produtos ?? data))
-    .catch(err => console.log(err));
+      .then(res => {
+        if (!res.ok) {
+          throw new Error("Erro HTTP" + res.status);
+        }
+        return res.json();
+      })
+      .then(data => setProdutos(data.produtos ?? data))
+      .catch(err => console.log(err));
   }, []);
 
-  const deleteProduto = (id) => {
-    fetch(`http://127.0.0.1:8000/produtos/${id}`,{method:'DELETE'})
-    .then(() =>{
-      setProdutos(produtos.filter(p => p.id !== id))
-      showNotification({
-            title: 'Sucesso!',
-            message: 'Produto Excluido com sucesso.',
+
+  const ativar = (produto) => {
+    if (produto.ativo === 'I') {
+      fetch(`http://127.0.0.1:8000/produtos/${produto.id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ ativo: 'A' }),
+      })
+        .then(() => {
+          showNotification({
+            title: 'Sucesso',
+            message: 'Produto Ativado com sucesso.',
             color: 'green',
-           styles: (theme) =>({
-                closeButton:{
-                    backgroundColor:'#202020',
-                },
-                root:{
-                    backgroundColor:'#202020',
-                    color:'#ffffffe5',
-                },
-                title:{
-                    color:'#fff',
-                },
+            styles: (theme) => ({
+              closeButton: {
+                backgroundColor: '#202020',
+              },
+              root: {
+                backgroundColor: '#202020',
+                color: '#ffffffe5',
+              },
+              title: {
+                color: '#fff',
+              },
             }),
             radius: 'md',
             autoClose: 3000,
-            icon: <IconCheck />, 
-      })
+            icon: <IconCheck />,
+          })
+        })
+    }
+    else{
+        showNotification({
+            title: 'Erro',
+            message: 'Produto Já esta Ativo.',
+            color: 'red',
+            styles: (theme) => ({
+              closeButton: {
+                backgroundColor: '#202020',
+              },
+              root: {
+                backgroundColor: '#202020',
+                color: '#ffffffe5',
+              },
+              title: {
+                color: '#fff',
+              },
+            }),
+            radius: 'md',
+            autoClose: 3000,
+            icon: <IconX />,
+          })
+    }
+  }
+
+  const inativar = (produto) => {
+    fetch(`http://127.0.0.1:8000/produtos/${produto.id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ ativo: 'I' }),
     })
-    
+      .then(() => {
+        showNotification({
+          title: 'Sucesso!',
+          message: 'Produto Inativo com sucesso.',
+          color: 'green',
+          styles: (theme) => ({
+            closeButton: {
+              backgroundColor: '#202020',
+            },
+            root: {
+              backgroundColor: '#202020',
+              color: '#ffffffe5',
+            },
+            title: {
+              color: '#fff',
+            },
+          }),
+          radius: 'md',
+          autoClose: 3000,
+          icon: <IconCheck />,
+
+        })
+      });
+  };
+
+
+
+  const deleteProduto = (id) => {
+    fetch(`http://127.0.0.1:8000/produtos/${id}`, { method: 'DELETE' })
+      .then(() => {
+        setProdutos(produtos.filter(p => p.id !== id))
+        showNotification({
+          title: 'Sucesso!',
+          message: 'Produto Excluido com sucesso.',
+          color: 'green',
+          styles: (theme) => ({
+            closeButton: {
+              backgroundColor: '#202020',
+            },
+            root: {
+              backgroundColor: '#202020',
+              color: '#ffffffe5',
+            },
+            title: {
+              color: '#fff',
+            },
+          }),
+          radius: 'md',
+          autoClose: 3000,
+          icon: <IconCheck />,
+        })
+      })
+
   }
 
   return (
     <>
-    <Navbar />
-    <div className='tabela'>
-      <div className='d-flex justify-content-between mb-2'>
-        <h2 className='fw-bold text-start texto-prin'>Produtos</h2>
-         
-         <div className='buttons d-flex'>
-          <div className='menu me-3'>
-          <Menu shadow='md'>
-            <Menu.Target>
-              <Button>Açoes</Button>
-              </Menu.Target>
-              <MenuDropdown>
-                <Menu.Item>Excluir</Menu.Item>
-                <Menu.Item>Ativar</Menu.Item>
-                <Menu.Item>Inativar</Menu.Item>
-              </MenuDropdown>
-          </Menu>
-          </div>
-          <div>
-          </div>
-          <Link to="/cadastro">
-            <Button variant="filled">Cadastrar</Button>
-          </Link>
-         </div>    
-      </div>
+      <Navbar />
+      <div className='tabela'>
+        <div className='d-flex justify-content-between mb-2'>
+          <h2 className='fw-bold text-start texto-prin'>Produtos</h2>
 
-       <Table style={{backgroundColor:'#202020cc'}} withRowBorders={false}>
-        <Table.Thead>
-          <Table.Tr style={{backgroundColor:'#202020'}}>
-            <Table.Th className='text-center'>Ativo</Table.Th>
-            <Table.Th className='text-center'>ID</Table.Th>
-            <Table.Th className='text-center' >Referencia</Table.Th>
-            <Table.Th className='text-center'>Descrição</Table.Th>
-            <Table.Th className='text-center'>Preço</Table.Th>
-            <Table.Th className='text-center'>Açoes</Table.Th>
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {produtos.map(produto => (
-            <Table.Tr key={produto.id}>
-              <Table.Td>{produto.ativo}</Table.Td>
-              <Table.Td >{produto.id}</Table.Td>
-              <Table.Td>{produto.referencia}</Table.Td>
-              <Table.Td>{produto.descricao}</Table.Td>
-              <Table.Td>{produto.preco}</Table.Td>
-              <Table.Td><Button size="xs" onClick={() => deleteProduto(produto.id)}>Excluir</Button></Table.Td>
+          <div className='buttons d-flex'>
+            <div className='menu me-3'>
+
+            </div>
+            <div>
+            </div>
+            <Link to="/cadastro">
+              <Button variant="filled">Cadastrar</Button>
+            </Link>
+          </div>
+        </div>
+
+        <Table style={{ backgroundColor: '#202020cc', borderRadius:'10px'}} withRowBorders={false}>
+          <Table.Thead style={{borderRadius:'10px'}}>
+            <Table.Tr style={{ backgroundColor: '#202020',borderRadius:'10px' }}>
+              <Table.Th className='text-center'>Ativo</Table.Th>
+              <Table.Th className='text-center'>ID</Table.Th>
+              <Table.Th className='text-center' >Referencia</Table.Th>
+              <Table.Th className='text-center'>Descrição</Table.Th>
+              <Table.Th className='text-center'>Preço</Table.Th>
+              <Table.Th className='text-center'>Açoes</Table.Th>
             </Table.Tr>
-          )
-          )}
-        </Table.Tbody>
-      </Table> 
-    </div>
+          </Table.Thead>
+          <Table.Tbody>
+            {produtos.map(produto => (
+              <Table.Tr key={produto.id}>
+                <Table.Td>
+                  {produto.ativo === 'A' ? (
+                    <ArchiveIcon color="#1f521fff" size={18} />
+                  ) : (
+                    <ArchiveIcon color="#ca1313ff" size={18} />
+                  )}
+                </Table.Td>
+                <Table.Td >{produto.id}</Table.Td>
+                <Table.Td>{produto.referencia}</Table.Td>
+                <Table.Td>{produto.descricao}</Table.Td>
+                <Table.Td>{produto.preco}</Table.Td>
+                <Table.Td>
+                  <Menu shadow='md'>
+                    <Menu.Target>
+                      <Button size='xs'>Açoes</Button>
+                    </Menu.Target>
+                    <MenuDropdown>
+                      <Menu.Item onClick={() => deleteProduto(produto.id)}>Excluir</Menu.Item>
+                      <Menu.Item onClick={() => ativar(produto)}>Ativar</Menu.Item>
+                      <Menu.Item onClick={() => inativar(produto)}>Inativar</Menu.Item>
+                    </MenuDropdown>
+                  </Menu>
+                </Table.Td>
+              </Table.Tr>
+            )
+            )}
+          </Table.Tbody>
+        </Table>
+      </div>
     </>
   )
 }
